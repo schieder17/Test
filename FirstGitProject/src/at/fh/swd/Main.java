1b12228ea60e26e61b103a642e6eee91427b864f
Markus Schieder <markus.schieder@edu.fh-joanneum.at>
2018-05-11 17:18:39 +0200

    Juhu

---
+++ FirstGitProject/src/at/fh/swd/Main.java
@@ -0,0 +1,18 @@
+package at.fh.swd;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+
+	    int a,b,c;
+
+	    a=1;
+	    b=1;
+	    c=0;
+
+	    c=a+b;
+	    System.out.println(c);
+
+    }
+}
